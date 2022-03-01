4a0856e470f42bec75f4e00063aeb616dbaa053d
PHIL <pan10020@gmail.com>
2022-03-02 02:19:55 +0800

    init

---
+++ src/views/DynamicRouter.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>
+        router
+    </h1>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  created () {
+    console.log(this.$route.params.id)
+    axios.get('https://randomuser.me/api/')
+      .then((res) => console.log(res))
+  }
+}
+</script>
